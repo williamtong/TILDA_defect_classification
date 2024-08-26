b6879434036d360053372bec38f448b171f7d8cf
will_tong2001@yahoo.com <will_tong2001@yahoo.com>
2024-08-26 00:48:15 -0700

    Added comments

--- TILDA-defect-classific/functions/models.py
+++ TILDA-defect-classific/functions/models.py
@@ -16,141 +16,122 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, multilabel_confusion_matrix
 import matplotlib.pyplot as plt
 
-label_dict = {0: 'objects', 1: 'hole', 2: 'oil spot', 3: 'thread error'}
-revlabel_dict = {'objects': 0, 'hole': 1, 'oil spot': 2, 'thread error': 3}
+from plotting import plot_ROC, plot_2class_histograms, plot_confusion_matrix
+
+label_dict = {0: 'objects', 1: 'hole', 2: 'oil_spot', 3: 'thread_error'}
+revlabel_dict = {'objects': 0, 'hole': 1, 'oil_spot': 2, 'thread_error': 3}
+
+aug_revlabel_dict = {'objects_augmented': 0, 'hole_augmented': 1, 'oil_spot_augmented': 2, 'thread_error_augmented': 3}
+
+auglabel_dict = {'objects': 'objects_augmented', 'hole': 'hole_augmented', 'oil_spot': 'oil_spot_augmented', 
+                 'thread_error': 'thread_error_augmented', 'good': 'good_augmented'}
 
 def get_class_weight(labels):
+    '''
+    This functions calculates the weighting factor for each class to be input into the model. 
+    For example if one class has a population of 9000 and the other class has a population of 1000, 
+    the weighting factor would be 0.1 for the first class and 0.9 for the second class.
+
+    input:
+    labals (list):  A list of the target variable for the data.
+
+    output:
+    class weight (dict): dictionary of class weights
+    '''
     class_weights = {}
     for label in set(labels):
         print(1/labels.count(label))
         class_weights[label] = 1/labels.count(label)
     return class_weights
+    
 
+def preprocessor(image, div_by_255 = False)):
+    '''
+    Function to format image to right size.  
+    Optional 
+    input:
+    image:  any size image
 
-def upsample(filepaths, revlabel_dict, label_dict, target_samples_per_class = None):
-
-    labels = [label for label in map(lambda filepath: filepath.split("/")[-2], filepaths)]
-    unique_labels = set(labels)
-        
-    label_counts = {}
-    for label in unique_labels:
-        label_counts[label] = labels.count(label)
-        
-    if target_samples_per_class is None:
-        target_samples_per_class = np.max(list(label_counts.values()))
-        
-    label_upsample_factors = {}
-    for label in unique_labels:
-        label_upsample_factors[revlabel_dict[label]] = target_samples_per_class/label_counts[label]
-        
-    upsampled_filepaths = []
-    for label in label_upsample_factors.keys():
-        label_filefaths = [label_filepath for label_filepath in filepaths if label_dict[label] in label_filepath]
-        label_filefaths = int(np.round(label_upsample_factors[label]))*label_filefaths
-        upsampled_filepaths.extend(label_filefaths)
-        # print(f'label = {label}, len(upsampled_filepaths) =  {len(upsampled_filepaths)}')
-    random.shuffle(upsampled_filepaths)
-    upsampled_labels = [revlabel_dict[label] for label in map(lambda filepath: filepath.split("/")[-2], 
-                                                           upsampled_filepaths)]
-    return upsampled_filepaths, upsampled_labels, label_upsample_factors 
-
-
-def preprocessor(image):
+    output:
+    image:  64x64 image
+    '''
     image = tf.io.read_file(image)
     image = tf.image.decode_jpeg(image, channels = 3)
     image = tf.image.resize(image, (64, 64))
-    # image = image/255
+    if div_by_255:
+        image = image/255  
     return image
 
 
-def plot_confusion_matrix(y_actual, y_pred, normalize = 'pred'):
+def map_path_to_augmented(old_feature_paths):
+    new_features_paths = []
+    for path in old_feature_paths:
+        label = path.split("/")[-2]
+        newlabel = auglabel_dict[label]
+        for counter in range(1,6):
+            # print(path, newlabel)
+            new_feature_path = path.replace(label, newlabel).replace('.png', '_monochrome_'+str(counter)+'.png').replace(" ","_")
+            new_features_paths.append(new_feature_path)
+    return new_features_paths
 
-    # y_actual = map(lambda label: labels_dict[label], y_actual)
-    # y_pred = map(lambda label: labels_dict[label], y_pred)
-    cm = confusion_matrix(y_true=y_actual, y_pred=y_pred)
-    labels = sorted(list(set(y_actual)))
-    print(cm)
-    mc_df = pd.DataFrame(cm,
-                         index=labels, 
-                         columns=labels 
-                        )
-    plt.title("Confusion Matrix (Counts)")
-    sns.heatmap(mc_df, annot =True, 
-                # fmt="d",
-                cmap=plt.get_cmap('Reds'))
-    plt.xticks(rotation = 90)
-    plt.yticks(rotation = 0)
-    plt.xlabel('y_pred')
-    plt.ylabel('y_actual')
-    plt.show()
     
-    if normalize == 'true':
-        plt.title("Confusion Matrix (Recall)")
-    elif normalize == 'pred':
-        plt.title("Confusion Matrix (Precision)")
-    elif normalize == 'all':
-        plt.title("Confusion Matrix (Accuracy)")
-    else:
-        return
-        
-    print('')
-    cm = confusion_matrix(y_true=y_actual, y_pred=y_pred, normalize=normalize)
-    mc_df = pd.DataFrame(cm,
-                         index=labels, 
-                         columns=labels
-                        )
-    sns.heatmap(mc_df, annot =True, 
-                # fmt="d",
-                color = 'r',
-                cmap=plt.get_cmap('Blues'))
-    plt.xticks(rotation = 90)
-    plt.yticks(rotation = 0)
-    plt.xlabel('y_pred')
-    plt.ylabel('y_actual')
-    plt.show()
-    return
-
 def recreate_labels(features, label_dict):
+    '''This function extracts the labels from the data paths, given the images' individual data paths. '''
     labels_text = [path.split("/")[-2] for path in features]
     labels_numerical = [label_dict[label_text] for label_text in labels_text]
     return labels_numerical
 
 
-def compile_image_model(filter_dim, dropout, conv_factor =1, patience = 20, lr =0.001, num_classes =5):
+def compile_image_model(filter_dim, dropout, num_filters = 4, patience = 20, lr =0.001, num_classes =5):
+    '''This function instantiation a CNN model with the follow specs
+    The CNN model will have three blocks.  For each subsequent block the number of filters doubles.
+    The last block is fully connected layer, followed by the output layer which is a softmax function.
+    The loss function is "sparse_categorical_crossentropy".
+    The optimizer is Adam.
+
+    input:
+    filter_dim (integer):      The dimension of a square convolutional filter.
+    dropout (0 ≤ float < 1):   Fraction connections that are dropped out for each block
+    num_filters (integer):     Number of filter for the starting block.  
+    patience (interger):       Number of epochs with no improvement after which training will be stopped. 
+    num_classes (integer):     Number of classes of the target variable
+
+    output:
+    image_learner:             Instantiated CNN model
+    '''
     call_back = EarlyStopping(patience = patience, restore_best_weights = True)
     image_learner = Sequential()
     
-    image_learner.add(Conv2D(filters = 4 * conv_factor, kernel_size = (filter_dim, filter_dim), 
-                             padding="same",strides = (1, 1), activation = "relu"))
-    image_learner.add(BatchNormalization(axis = 3, name = 'bn0'))
-    image_learner.add(Dropout(dropout))
-    image_learner.add(MaxPool2D())
+    image_learner.add(Conv2D(filters = int(num_filters), kernel_size = (filter_dim, filter_dim), 
+                             padding="same",strides = (1, 1), activation = "relu", name = '2dfilter0'))
+    print(f'image_learner.layers[0].filters: {image_learner.layers[0].filters}')
+    image_learner.add(BatchNormalization(axis = 2, name = 'batchnormalization0'))
+    image_learner.add(Dropout(dropout, name = 'dropout0'))
+    image_learner.add(MaxPool2D(name = 'maxpool0'))
     
-    image_learner.add(Conv2D(filters = 8 * conv_factor, kernel_size = (filter_dim, filter_dim), 
-                             padding="same",strides = (1, 1), activation = "relu"))
-    image_learner.add(BatchNormalization(axis = 3, name = 'bn1'))
-    image_learner.add(Dropout(dropout))
-    image_learner.add(MaxPool2D())
+    num_filters *= 2
+    image_learner.add(Conv2D(filters = int(num_filters), kernel_size = (filter_dim, filter_dim), 
+                             padding="same",strides = (1, 1), activation = "relu", name = '2dfilter1'))
+    print(f'image_learner.layers[4].filters: {image_learner.layers[4].filters}')
+    image_learner.add(BatchNormalization(axis = 2, name = 'batchnormalization1'))
+    image_learner.add(Dropout(dropout, name = 'dropout1'))
+    image_learner.add(MaxPool2D(name = 'maxpool1'))
     
-    image_learner.add(Conv2D(filters = 16 * conv_factor, kernel_size = (filter_dim, filter_dim), 
-                             padding="same", strides = (1, 1), activation = "relu"))
-    image_learner.add(BatchNormalization(axis = 3, name = 'bn2'))
-    image_learner.add(Dropout(dropout))
-    image_learner.add(MaxPool2D())
+    num_filters *= 2
+    image_learner.add(Conv2D(filters = int(num_filters), kernel_size = (filter_dim, filter_dim), 
+                             padding="same", strides = (1, 1), activation = "relu", name = '2dfilter2'))
+    print(f'image_learner.layers[8].filters: {image_learner.layers[8].filters}')
+    image_learner.add(BatchNormalization(axis = 2, name = 'batchnormalization2'))
+    image_learner.add(Dropout(dropout, name = 'dropout2'))
+    image_learner.add(MaxPool2D(name = 'maxpool2'))
     
-    image_learner.add(Conv2D(filters = 32 * conv_factor, kernel_size = (filter_dim, filter_dim),
-                             padding="same", strides = (1, 1), activation = "relu"))
-    image_learner.add(BatchNormalization(axis = 3, name = 'bn3'))
-    image_learner.add(Dropout(dropout))
-    image_learner.add(MaxPool2D())
-    
-    image_learner.add(Flatten())
-    image_learner.add(Dense(units = 32 * conv_factor, activation = "relu"))
-    image_learner.add(Dense(units = num_classes, activation = "softmax"))
+    num_filters /= 4
+    image_learner.add(Flatten(name = 'flatten'))
+    image_learner.add(Dense(units = int(num_filters), activation = "relu", name = 'fully_connected'))
+    image_learner.add(Dense(units = num_classes, activation = "softmax", name = 'output'))
     
     image_learner.compile(loss = "sparse_categorical_crossentropy", metrics = ["accuracy"], 
                           optimizer = tf.keras.optimizers.Adam(learning_rate= lr))
-    print(image_learner.layers)
     return image_learner
 
 class CNN_model: 
@@ -158,88 +139,102 @@ class CNN_model:
     trains a model, and and outputs the training scores.
     '''
 
-    def __init__(self, filter_dim, dropout, patience = 5, conv_factor = 1, lr = 0.001, num_classes = 5):
+    def __init__(self, filter_dim =8 , dropout = 0.1, num_filters = 4, lr = 0.001, patience = 5, num_classes = 4):
         '''
         input:
-        Model (sklearn.ensemble or xgboost class): a tree tree.
-        params (dict):  parameters to be input into the model
-        price_max (int or float): max for the output plot (only).  Not used in model and culling data.
+        filter_dim (integer):      The dimension of a square convolutional filter.
+        dropout (0 ≤ float < 1):   Fraction connections that are dropped out for each block
+        num_filters (integer):     Number of filter for the starting block.  
+        patience (interger):       Number of epochs to elapse  Number of epochs with no improvement after which training will be stopped. 
+        num_classes (integer):     Number of classes of the target variable
         '''
         self.filter_dim = filter_dim,  
         self.dropout = dropout
-        self.conv_factor = conv_factor
         self.lr = lr
         self.call_back = EarlyStopping(monitor="val_accuracy", 
                               verbose=1, 
                               patience = patience, 
                               restore_best_weights = True,
-                              start_from_epoch= 10)
-        self.image_learner = compile_image_model(filter_dim, dropout, conv_factor = 1, lr = self.lr, num_classes =num_classes)
-        # return self
+                              start_from_epoch= 150)
+        self.num_classes = num_classes
+        self.image_learner = compile_image_model(filter_dim, dropout, num_filters = num_filters, lr = self.lr, num_classes =num_classes)
 
-    def fit(self, train_set, eval_set, label_dict, class_weights=None, epochs = 100000, verbose='auto'):
+    def fit(self, features_train, labels_train, features_eval, labels_eval, label_dict, batch_size=128, class_weights=None, epochs = 100000, verbose=2):
         '''
-        input:
-        X_train (pandas Dataframe): X data for training
-        y_train_actual (pandas Dataframe or numpy array (1d): y data for training
+        This uses the image_learner model to fit the data set. It uses the the eval data set to montior overfitting,
+        and stops the fitting after "patience" number of epochs with no improvement. 
+        Then it uses the eval data set to plot the confusion matrices.
         
-        output:
-        model: trained model
+        input:
+        features_train (numpy array):  An M x M x N array of training set images, N x N are the images' dimension.  There are M images.
+        labels_train (numpy array):    N x 1 array of target variable for the training set of the images.    
+        features_eval(numpy array):    An M x M x P array of evalution set images, N x N are the images' dimension.  There are P images.
+        labels_eval (numpy array):     P x 1 array of target variables for the evalution set of the images. 
+        label_dict (dict):             label dictionary to tell the labels are.  For example, {0: "good", 1: "defect"}
+        batch_size (integer):          minibatch size for stochastic gradient descent
+        class_weights (dict):          See explanation for get_class_weight function.
+        epochs (integer):              Max number of epoch if not early stopped.
+        verbose (0, 1, 2):             Verbosity mode, 0 or 1. Mode 0 is silent, and mode 1 displays messages when the callback takes an action.
+
+        outputs:
+        history:        accuracy and validation accuracy
+        image_learner:  trained model
         '''
         self.label_dict = label_dict
-        print(self.image_learner.summary())
-        self.history = self.image_learner.fit(train_set, 
-                                              validation_data = [eval_set], 
+        self.history = self.image_learner.fit(features_train, labels_train,
+                                              validation_data = [features_eval, labels_eval], 
                                               callbacks = [self.call_back], 
-                                              epochs = epochs, 
+                                              epochs = epochs,  batch_size = batch_size,
                                               class_weight=class_weights,
                                               verbose=verbose
                                              )
         
-        #Calculate Training error.    
+        print(self.image_learner.summary())r.    
         print("Finished fitting.  Predicting X...")
-        images_eval = [image for image, label in eval_set.as_numpy_iterator()][0]
-        y_predict = self.image_learner.predict(images_eval)
-        # print(f'1 y_predict = {y_predict}')
+        y_predict = self.image_learner.predict(features_eval)
         y_predict = [self.label_dict[y.argmax()] for y in y_predict]
-        # print(f'2 y_predict = {y_predict}')
-        print("Finished predicting X.")
-        y_actual = [label for image, label in eval_set.as_numpy_iterator()][0]
-        # print(f'1 y_actual = {y_actual}')
-        y_actual = [self.label_dict[label] for label in y_actual]
-        # print(f'2 y_actual = {y_actual}')
+        print("Finished predicting X with eval data set.")
+        y_actual = [self.label_dict[label] for label in labels_eval]
 
         #Plot training results
+        plot_confusion_matrix(y_actual, y_predict, normalize = None)
+        plot_confusion_matrix(y_actual, y_predict, normalize = 'all')
         plot_confusion_matrix(y_actual, y_predict, normalize = 'pred')
+        plot_confusion_matrix(y_actual, y_predict, normalize = 'true')
 
-        
         return self.history, self.image_learner
         
-    def predict(self, test_set):
+    def predict(self, features_test, labels_test = None):
 
         '''
+        The uses the image_learner model to predict the images data set.
+        If the actual labels are supplied, it will plot the confusion matrices.
+        If the num_class == 2, it will plot the AUC-ROC curve and the 2 class probability distribution histograms.
+
+        
         input:
-        X (pandas Dataframe):  X data to be predicted.
-                               If there's only one row, input in the following format.
-                               X.iloc[row_number, row_number + 1, :]
-        y (pandas Dataframe or np array:  y data
-
+        features_test (numpy array):  An M x M x Q array of training set images, N x N are the images' dimension.  There are Q images.
+        labels_test (numpy array):    Q x 1 array of target variable for the training set of the images.    
+     
         output:
-        y_predict
+        y_predict:  The class with the max probability
+        y_predict_prob:  The predicted probabilities.
         '''
-        # If y_actual is not None, then calculate the holdout errors and plot results
 
-        print("Finished fitting.  Predicting X...")
-        images_test = [image for image, label in test_set.as_numpy_iterator()][0]
-        y_predict_prob = self.image_learner.predict(images_test)
-        # print(f'1 y_predict = {y_predict}')
+        y_predict_prob = self.image_learner.predict(features_test)
         y_predict = [self.label_dict[y.argmax()] for y in y_predict_prob]
-        # print(f'2 y_predict = {y_predict}')
+        
         print("Finished predicting X.")
-        y_actual = [label for image, label in test_set.as_numpy_iterator()][0]
-        # print(f'1 y_actual = {y_actual}')
-        y_actual = [self.label_dict[label] for label in y_actual]
-        # print(f'2 y_actual = {y_actual}')
-        #Ploting holdout results
-        plot_confusion_matrix(y_actual, y_predict, normalize = 'pred')
+                                                    
+        # If y_actual is not None, then calculate the holdout errors and plot results
+        if labels_test is not None:
+            y_actual = [self.label_dict[label] for label in labels_test]
+            #Ploting holdout results
+            plot_confusion_matrix(y_actual, y_predict, normalize = None)
+            plot_confusion_matrix(y_actual, y_predict, normalize = 'all')
+            plot_confusion_matrix(y_actual, y_predict, normalize = 'pred')
+            plot_confusion_matrix(y_actual, y_predict, normalize = 'true')
+            if self.num_classes == 2:
+                plot_ROC(labels_test, y_predict_prob[:,1])
+                plot_2class_histograms(labels_test, y_predict_prob, self.label_dict, stepsize = 0.01, density = True, semilog = False, holdout = True)   
         return y_predict, y_predict_prob
--- TILDA-defect-classific/functions/plotting.py
+++ TILDA-defect-classific/functions/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, multilabel_confusion_matrix, roc_curve, auc, RocCurveDisplay
 
 
-def plot_ROC(y_actual, y_pred, estimator_name='2-class CNN model'):
+def plot_ROC(y_actual, y_pred, filename = None, estimator_name='2-class CNN model'):
     fpr, tpr, thresholds = roc_curve(y_actual,y_pred)
     roc_auc = auc(fpr, tpr)
     display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, 
@@ -11,23 +11,113 @@ def plot_ROC(y_actual, y_pred, estimator_name='2-class CNN model'):
     display.plot()
     plt.title("Receiver Operating Characteristics (ROC)")
     plt.grid(ls = ':')
+    if filename is not None:
+        plt.savefig(filename)
     plt.show()
 
 
-def plot_2class_histograms(y_actual, y_predict, label_dict, stepsize = 0.01, density = False):
+def plot_2class_histograms(y_actual, y_predict, label_dict, stepsize = 0.01, density = False, semilog = False, holdout = False):
     y_prob = y_predict[:,1]
     y_actual_labels =  np.array([y for y in map(lambda number: label_dict[number], list(y_actual))])
     unique_labels = list(set(y_actual_labels))
     bins = np.arange(0,1+stepsize, stepsize)
-
-    plt.title("Distributions of Predicted Probabilities")
+    if holdout:
+        plt.title("Distributions of Predicted Probabilities (holdout)")
+    else: 
+        plt.title("Distributions of Predicted Probabilities")
     for unique_label in unique_labels:
         plot_label_samples = y_prob[y_actual_labels == unique_label]
         plt.hist(plot_label_samples, label = unique_label, bins = bins, alpha = 0.5, density = density)
     plt.xlabel("y_predict (probability)")
-    plt.ylabel("frequency")
+    plt.ylabel("Probability Density (%)")
+    if semilog:
+        plt.semilogy()
     plt.xticks(np.arange(0,1.1,0.1))
     plt.xlim(-0.01,1.01)
     plt.grid(ls = ':')
     plt.legend()
     plt.show()
+
+
+def plot_CNN_filters(image_learner, caption, layer_num, path = "../notebook_CNN/plots/",
+                     filename = '2_class_4L_16x32.png', dpi = 'figure', size = 100, max_num_rows = 32):
+    conv2d_16_layer = image_learner.layers[layer_num]
+    vmin = conv2d_16_layer.kernel.numpy().min()
+    vmax = conv2d_16_layer.kernel.numpy().max()
+    
+    d0 = conv2d_16_layer.kernel.shape[2]
+    d1 = conv2d_16_layer.kernel.shape[3]
+    print(d0, d1, int(d0*d1/8))
+    if d1 <= 8:
+        fig, ax = plt.subplots(1, 8, figsize=(100,100), layout="compressed")
+        for col in range(d1):
+            filters_0 = conv2d_16_layer.kernel[:,:,0,col]
+            # print(f'plot positions: col: {col}, index: {col}')
+            ax[col].imshow(filters_0, cmap = 'gray')
+            ax[col].set_xticklabels([], fontsize = 0)
+            ax[col].set_yticklabels([], fontsize = 0)
+            ax[col].xaxis.set_ticks_position('none') 
+            ax[col].yaxis.set_ticks_position('none') 
+            ax[col].text(0, 1, caption, size= size, color = 'yellow', weight='bold')
+        # print()
+    else:
+        num_rows = int(d0*d1/8)
+        if num_rows > max_num_rows:
+            num_rows = max_num_rows
+        fig, ax = plt.subplots(num_rows, 8, figsize=(100,100), layout="compressed")
+        for indx0 in range(d0):
+            for indx1 in range(d1):
+                filters_0 = conv2d_16_layer.kernel[:,:,indx0,indx1]
+                col = indx1%8
+                row = int((indx0*2*d0+indx1)/8)
+                if row + 1 <= max_num_rows:
+                    # print(f'plot positions: row: {row}, col: {col}, layer indices: {indx0, indx1}')
+                    ax[row, col].imshow(filters_0, cmap = 'gray', vmin = vmin, vmax = vmax)
+                    ax[row, col].set_xticklabels([], fontsize = 0)
+                    ax[row, col].set_yticklabels([], fontsize = 0)
+                    ax[row, col].xaxis.set_ticks_position('none') 
+                    ax[row, col].yaxis.set_ticks_position('none') 
+                    ax[row, col].text(0, 1, caption, size= size, color = 'yellow', weight='bold')
+            # print()
+    if filename is not None:
+        plt.savefig(path + filename, dpi = dpi)
+    plt.show()
+
+def plot_confusion_matrix(y_actual, y_pred, normalize = 'pred'):
+
+    labels = sorted(list(set(y_actual)))
+    
+    if normalize == 'true':
+        plt.title("Confusion Matrix (Recall)")
+        cmap = 'Greens'
+    elif normalize == 'pred':
+        plt.title("Confusion Matrix (Precision)")
+        cmap = 'Blues'
+    elif normalize == 'all':
+        plt.title("Confusion Matrix (Accuracy)")
+        cmap = 'Purples'
+    elif normalize is None:
+        plt.title("Confusion Matrix (Counts)")
+        cmap = 'Reds'
+    else:
+        return
+        
+    print('')
+    cm = confusion_matrix(y_true=y_actual, y_pred=y_pred, normalize=normalize)
+    print(cm)
+    num_of_diagonal = np.sum([cm[x,x] for x in range(cm.shape[0])])
+    print(f"sum of diagonal = {num_of_diagonal}")
+    mc_df = pd.DataFrame(cm,
+                         index=labels, 
+                         columns=labels
+                        )
+    sns.heatmap(mc_df, annot =True, 
+                # fmt="d",
+                color = 'r',
+                cmap=plt.get_cmap(cmap))
+    plt.xticks(rotation = 90)
+    plt.yticks(rotation = 0)
+    plt.xlabel('y_pred')
+    plt.ylabel('y_actual')
+    plt.show()
+    return
